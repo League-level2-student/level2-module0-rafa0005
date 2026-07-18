331cf76101d645e475d39edf6f7e37bc85213694
Aarit Jayaswal <aaritrj@gmail.com>
2026-07-18 12:44:17 -0700

    bob

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,16 +3,19 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
+		for(int i = 0; i < 5; i++) {
+		
+		String[]movies = {"LordOfTheRings", "HarryPotter", "Marvel", "StarWars", "DC"};
 		
 		//2. print the third element in the array
-		
+		System.out.println("Marvel");
 		//3. set the third element to a different value
-		
+		movies[2] = "TopGun";
 		//4. print the third element again
-		
+		System.out.println("TopGun");
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		}
 		
 		//6. make an array of 50 integers
 		
